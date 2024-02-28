a189b0a0f26046264d87ed46c24bb920b5bc06e2
AhmedHosny1998 <hosny4620Qgmail.com>
2024-02-27 16:07:03 -0800

    dd

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-file { '/tmp/school':
-	ensure => file,
-	path => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'	
+# create a file in /tmp
 
-
+file {  '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
