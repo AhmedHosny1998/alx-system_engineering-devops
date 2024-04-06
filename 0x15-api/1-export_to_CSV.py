15cf4d19a167e9e59dee2a105c3f09c19b474766
Ahmed Hosny <hosny4620@gmail.com>
2024-04-06 15:32:46 +0200

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = argv[1]
+    user_id = argv[0]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todo", params={"userid": user_id}).json()
     with open("{}.csv".format(user_id), "w") as csvfile:
