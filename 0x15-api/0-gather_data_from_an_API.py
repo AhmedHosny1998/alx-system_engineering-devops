b13d23640d5d5259f9c5d3639f5a26cded94252f
Ahmed Hosny <hosny4620@gmail.com>
2024-04-06 15:36:07 +0200

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
+    user_id = sys.argv[0]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos", params={"userId": user_id}).json()
     completed = [task for task in todo if task.get("completed") is True]
