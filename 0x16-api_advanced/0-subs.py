8989fef155176820eff983339bae0c7b920c988c
Ahmed Hosny <hosny4620@gmail.com>
2024-04-11 04:06:43 +0200

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ BASE_URL = 'http://reddit.com/r/{}/about.json'
 
 def number_of_subscribers(subreddit):
     '''Gets number of reddit subscribers'''
-    headers = {'User-agent': 'Unix:0-subs:v1'}
+    headers = {'User-agent': 'Mozilla/5.0/API'}
     response = requests.get(BASE_URL.format(subreddit),
                             headers=headers)
     if response.status_code != 200:
